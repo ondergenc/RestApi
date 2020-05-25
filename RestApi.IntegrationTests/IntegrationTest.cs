d1153f4b662862ce1558645b472f47d76d41b176
Onder Genc <ondergenc@gmail.com>
2020-05-25 12:45:19 +0300

    Corrupted test integration fixed.

--- RestApi.IntegrationTests/IntegrationTest.cs
+++ RestApi.IntegrationTests/IntegrationTest.cs
@@ -1,10 +1,74 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using RestApi.Data;
+using RestApi.Contracts.V1;
+using RestApi.Contracts.V1.Requests;
+using RestApi.Contracts.V1.Responses;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Linq;
+
 namespace RestApi.IntegrationTests
 {
-    public class IntegrationTest
+    public class IntegrationTest : IDisposable
     {
-        public IntegrationTest()
+        protected readonly HttpClient TestClient;
+        private readonly IServiceProvider _serviceProvider;
+
+        protected IntegrationTest()
         {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        //services.RemoveAll(typeof(DataContext));
+                        var descriptor = services.SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<DataContext>));
+                        if (descriptor != null)
+                        {
+                            services.Remove(descriptor);
+                        }
+                        services.AddDbContext<DataContext>(options => { options.UseInMemoryDatabase("TestDb"); });
+                    });
+                });
+
+            _serviceProvider = appFactory.Services;
+            TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJwtAsync());
+        }
+
+        private async Task<string> GetJwtAsync()
+        {
+            var model = new UserRegistrationRequest
+            {
+                Email = "test@integration.com",
+                Password = "SomePass1234!"
+            };
+            var response = await TestClient.PostAsJsonAsync(ApiRoutes.Identity.Register, model);
+            var registrationResponse = await response.Content.ReadAsAsync<AuthSuccessResponse>();
+
+            return registrationResponse.Token;
+        }
+
+        protected async Task<PostResponse> CreatePostAsync(CreatePostRequest request)
+        {
+            var response = await TestClient.PostAsJsonAsync(ApiRoutes.Posts.Create, request);
+            return await response.Content.ReadAsAsync<PostResponse>();
+        }
+
+        public void Dispose()
+        {
+            using var serviceScope = _serviceProvider.CreateScope();
+            var context = serviceScope.ServiceProvider.GetService<DataContext>();
+            context.Database.EnsureDeleted();
         }
     }
 }
